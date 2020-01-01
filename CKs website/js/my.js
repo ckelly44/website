1f2debfcb10c89f26fa822b4004c1df5a4b697b5
ckelly44 <colinedwardkelly@gmail.com>
2020-01-01 17:01:49 -0500

    Organized js

--- CKs website/js/my.js
+++ CKs website/js/my.js
@@ -1,52 +1,84 @@
-var selector;
-var x = 1;
-var y = 1;
-var inViewCounter = 0;
-window.addEventListener('keydown', function(e){
-	const key = e.keyCode;
-	if(key === 39 && inViewCounter !== 3) {
-		document.getElementById("p"+ ++inViewCounter).scrollIntoView({behavior: "smooth"});
-	}
-	if(key === 37 && inViewCounter !== 0) {
-		document.getElementById("p"+ --inViewCounter).scrollIntoView({behavior: "smooth"});
-	}
-	else
-		return;
+/*
+
+
+	On load handling
+
+
+*/
+
+//on load make first row visible
+$(document).ready(function(){
+	var j = document.querySelector('.is-hidden');
+	j.classList.remove('is-hidden');
+	j.ontransitionend = () => {
+		if(event.propertyName === 'transform'){
+			j = document.querySelector('.is-hidden');
+			j.classList.remove('is-hidden');
+		};
+	};
 })
+
+/*
+
+
+	'Learn more' preview handling
+
+
+*/
+
+var selector = null; //last selected 'learn more' preview element
+
+//show 'learn more' elements on click
 function show(intValue) {
 	var id = "test" + intValue;
-	var x = document.getElementById(id);
-	x.style.display = "block";
-	selector = x;
+	selector = document.getElementById(id);
+	selector.style.display = "block";
 }
+//exit 'learn more' elements on click
 function exitDiv() {
-	$(selector).hide();
+	selector.style.display = "none";
 }
-$(document).ready(function(){
-	$("#i0").css('visibility', 'visible').hide().fadeIn(2000);
-	var j = document.querySelector('.is-preload');
-	console.log(j);
-	j.classList.remove('is-preload');
-});
-$(document).mouseup(function(e) 
-{
-    var container = $(selector);
-    // if the target of the click isn't the container nor a descendant of the container
-    if (!container.is(e.target) && container.has(e.target).length === 0) 
-    {
-        container.hide();
-    }
-});
+//close 'learn more' element if user clicks outside of the element
+document.addEventListener('mouseup', function(e) {
+	if(selector === null) return;
+	if(selector.style.display === "block" && !selector.contains(e.target))
+		selector.style.display = "none";
+})
+
+/*
+
+	
+	Scroll handling
+
+
+*/
+
+var x = 1; //counter for element scroll animations
+
+//make elements visible when scrolled to
 $(window).scroll(function() {
-	var trigger = y * $("#p0").height() - .75* $(window).height();
+	var trigger = x * $("#p0").height() - .75* $(window).height();
 	var id = "#i" + x;
 	if($(window).scrollTop() >= trigger && x < 4) {
-		var j = document.querySelector('.is-preload');
-		j.classList.remove('is-preload');
-		$(id).css('visibility', 'visible').hide().fadeIn(2000);
+		var j = document.querySelector('.is-hidden');
+		j.classList.remove('is-hidden');
+		j = document.querySelector('.is-hidden');
+		j.classList.remove('is-hidden');
+		//$(id).css('visibility', 'visible').hide().fadeIn(2000);
 		x++;
-		y += 1;
 		if(x == 4){
 		$(this).off('scroll');}
 	}
-});
+})
+
+var inViewCounter = 0; //element that is currently in view of window
+
+//left and right arrows will bring into view previous and next elements
+window.addEventListener('keydown', function(e){
+	if(e.keyCode === 39 && inViewCounter !== 3) {
+		document.getElementById("p"+ ++inViewCounter).scrollIntoView({behavior: "smooth"});
+	}
+	if(e.keyCode === 37 && inViewCounter !== 0) {
+		document.getElementById("p"+ --inViewCounter).scrollIntoView({behavior: "smooth"});
+	}
+})
